bfa066de8545969041607f03fddb5cd29b3c5a99
genericFJS <freejailsymphonic@yahoo.de>
2022-01-18 23:40:52 +0100

    Add typography static adapter bug example

---
+++ src/lib/JsTypography.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { onMount } from 'svelte';
+
+	import screenshot from '../assets/screenshot-vite-static-typography-build.png';
+
+	let content;
+
+	onMount(() => {
+		content = 'Hello world from Javascript typography';
+	});
+</script>
+
+<div class="prose">
+	<h3>{content}</h3>
+	<p>
+		This should be prose. And it is (if you don't mind the vite <code>Unused CSS selector</code> warnings).
+	</p>
+	<p>But… oh no! It can no longer be build with the static adapter:</p>
+	<img src={screenshot} alt="" />
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,9 +3,11 @@
 	import TsWithStyle from '../lib/TsWithStyle.svelte';
 	import JsComponent from '../lib/JsComponent.svelte';
 	import TsWithStyleOneLine from '$lib/TsWithStyleOneLine.svelte';
+	import JsTypography from '$lib/JsTypography.svelte';
 </script>
 
 <JsComponent />
 <TsComponent />
 <TsWithStyle />
 <TsWithStyleOneLine />
+<JsTypography />
